4f6dfb4890ce05b29129dc21ee6127f82825cbe3
mentOS31 <50947403+mentOS31@users.noreply.github.com>
2022-03-06 15:28:32 +0900

    Update api.py

--- api.py
+++ api.py
@@ -2,9 +2,12 @@ from flask import Flask, jsonify
  
 app = Flask(__name__)
  
-@app.route('/')
-def hello_world():
+@app.route('/data/', method=["PUT"])
+def put_user():
+    user = request.json["user_id"]
+    password = request.json["password"]
     return jsonify({'message': 'Hello world'})
  
+ 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8888, debug=True)
+    app.run()
